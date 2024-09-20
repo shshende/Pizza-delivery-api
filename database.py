e05154d09a6b6928948a9df6edd6f7c8c1305301
shshende <shantanu.shende@globallogic.com>
2024-09-20 17:36:02 +0530

    Update database.py

--- database.py
+++ database.py
@@ -9,3 +9,10 @@ engine=create_engine('postgresql://postgres:nathanoj35@localhost/pizza_delivery'
 Base=declarative_base()
 
 Session=sessionmaker()
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
